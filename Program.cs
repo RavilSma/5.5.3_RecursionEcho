0eb40d566f747b5b1fafbee672b99bc3d36a059c
RavilSma <smakaev00@mail.ru>
2023-12-13 00:35:18 +0300

    Активировано задание 5.5.3-5.5.4

--- Program.cs
+++ Program.cs
@@ -10,35 +10,35 @@ namespace _5._5._3_RecursionEcho
     {
         #region 5.5.3 - 5.5.4 Рекурсия имитации эха и окрашивание фона текста консоли
 
-        //static void Main(string[] args)
-        //{
-        //    Console.WriteLine("Напишите что-то (не менее 2 символов): ");
-        //    string str = Console.ReadLine();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите что-то (не менее 2 символов): ");
+            string str = Console.ReadLine();
 
-        //    Console.WriteLine("Укажите глубину эха: ");
-        //    int deep = int.Parse(Console.ReadLine());
+            Console.WriteLine("Укажите глубину эха: ");
+            int deep = int.Parse(Console.ReadLine());
 
-        //    Echo(str, deep);
+            Echo(str, deep);
 
-        //    Console.ReadKey();
-        //}
+            Console.ReadKey();
+        }
 
-        //static void Echo(string str, int deep)
-        //{
-        //    var strModif = str;
-        //    if (strModif.Length > 2)
-        //    {
-        //        strModif = strModif.Remove(0, 2);
+        static void Echo(string str, int deep)
+        {
+            var strModif = str;
+            if (strModif.Length > 2)
+            {
+                strModif = strModif.Remove(0, 2);
 
-        //        Console.BackgroundColor = (ConsoleColor)deep;
-        //        Console.WriteLine("..." + strModif);
+                Console.BackgroundColor = (ConsoleColor)deep;
+                Console.WriteLine("..." + strModif);
 
-        //        if (deep > 1)
-        //        {
-        //            Echo(strModif, deep - 1);
-        //        }
-        //    }
-        //}
+                if (deep > 1)
+                {
+                    Echo(strModif, deep - 1);
+                }
+            }
+        }
 
         #endregion
 
@@ -64,25 +64,25 @@ namespace _5._5._3_RecursionEcho
 
         #region 5.5.8 Возведение в степень
 
-        public static void Main(string[] args)
-        {
-            Console.Write("Введите целое число для возведения в степень: ");
-            int n = int.Parse(Console.ReadLine());
+        //public static void Main(string[] args)
+        //{
+        //    Console.Write("Введите целое число для возведения в степень: ");
+        //    int n = int.Parse(Console.ReadLine());
 
-            Console.Write("Введите целое число степени: ");
-            byte pow = byte.Parse(Console.ReadLine());
+        //    Console.Write("Введите целое число степени: ");
+        //    byte pow = byte.Parse(Console.ReadLine());
 
-            Console.WriteLine($"Полученный результат: {Exponentiation(n, pow)}");
+        //    Console.WriteLine($"Полученный результат: {Exponentiation(n, pow)}");
 
-            Console.ReadKey();
-        }
+        //    Console.ReadKey();
+        //}
 
-        public static decimal Exponentiation(int n, byte pow)
-        {
-            if (pow == 0) return 1;
-            else if (pow == 1) return n;
-            else return n * Exponentiation(n, --pow);
-        }
+        //public static decimal Exponentiation(int n, byte pow)
+        //{
+        //    if (pow == 0) return 1;
+        //    else if (pow == 1) return n;
+        //    else return n * Exponentiation(n, --pow);
+        //}
 
         #endregion
 
